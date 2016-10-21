ad1bc3e3cf86c68e336e2906be2f0593e1d03658
Vitaliy Trofymiuk <vitaliy.trofymiuk@onapp.com>
2016-10-21 09:18:16 +0300

    Prepare to Rails 4.2

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 echo "*** Running onapp-utils specs"
 
+
 bundle install                                      || exit 1
 bundle exec rake app:db:drop app:db:create          || exit 1
 bundle exec rake app:db:migrate app:db:test:prepare || exit 1
