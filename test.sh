8dd8ce2b0b10626eac2064f48346a5fe2e79f9bd
Volodymyr Byno <volodymyr.byno@gmail.com>
2016-11-07 14:27:33 +0200

    Update activerecord dependency to 4.2.6
    Change configuration keys;
    Update tests;
    Get rid of attr_accessible
    Change test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 
 echo "*** Running onapp-utils specs"
 
-
 bundle install                                      || exit 1
 bundle exec rake app:db:drop app:db:create          || exit 1
 bundle exec rake app:db:migrate app:db:test:prepare || exit 1
