402a2685eaf4e8724ba3cf4341d045b524db5ab8
Vitaliy Nahaylo <nahaylo@gmail.com>
2016-12-02 17:37:23 +0200

    Add CentOSVersion class and remove not_centos5 from RedhatRelease

---
+++ lib/utils/centos_version.rb
@@ -0,0 +1,49 @@
+module Utils
+  class CentOSVersion
+    attr_reader :version
+
+    def initialize(version)
+      @version = version.to_i
+    end
+
+    class << self
+      def centos5?(*args)
+        new(*args).centos5?
+      end
+      alias_method :five?, :centos5?
+
+      def centos6?(*args)
+        new(*args).centos6?
+      end
+      alias_method :six?, :centos6?
+
+      def centos7?(*args)
+        new(*args).centos7?
+      end
+      alias_method :seven?, :centos7?
+
+      def not_centos5?(*args)
+        new(*args).not_centos5?
+      end
+    end
+
+    def centos5?
+      version == 5
+    end
+    alias_method :five?, :centos5?
+
+    def centos6?
+      version == 6
+    end
+    alias_method :six?, :centos6?
+
+    def centos7?
+      version == 7
+    end
+    alias_method :seven?, :centos7?
+
+    def not_centos5?
+      !centos5?
+    end
+  end
+end
