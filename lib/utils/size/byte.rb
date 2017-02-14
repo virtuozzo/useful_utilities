b0348df0ff6dc80644f4644d4e8c31e2490c714a
Volodymyr Vysochanskyi <volodymyr.vysochanskyi@onapp.com>
2017-02-14 18:43:48 +0200

    CORE-8837 Add Decimal & Binary Standards. Fulfill with rspec tests. Bump version to  5.4.0

--- lib/utils/size/byte.rb
+++ lib/utils/size/byte.rb
@@ -1,77 +1,65 @@
+require 'utils/size/standard/binary'
+
 module Utils
   module Size
     module Byte
-      KILOBYTE = 1.kilobyte
-      MEGABYTE = 1.megabyte
-      GIGABYTE = 1.gigabyte
-      TERABYTE = 1.terabyte
+      # @note: used ISO standard http://en.wikipedia.org/wiki/Binary_prefix
+      #   Binary
+      #   1 kilobyte = 1024 byte
+      include Utils::Size::Standard::Binary
 
-      # @note: used JEDEC standarts http://en.wikipedia.org/wiki/Binary_prefix
+      HALF_OF_SECTOR = 0.5
+
       def to_terabytes(size, unit)
-        if    unit == :B  then size.fdiv(TERABYTE)
-        elsif unit == :KB then size.fdiv(GIGABYTE)
-        elsif unit == :MB then size.fdiv(MEGABYTE)
-        elsif unit == :GB then size.fdiv(KILOBYTE)
-        elsif unit == :TB then size
-        else  unsupported_unit!(unit)
-        end
+        to_tebi(size, byte_prefix(unit))
       end
 
       def to_gigabytes(size, unit)
-        if    unit == :B  then size.fdiv(GIGABYTE)
-        elsif unit == :KB then size.fdiv(MEGABYTE)
-        elsif unit == :MB then size.fdiv(KILOBYTE)
-        elsif unit == :GB then size
-        elsif unit == :TB then size * KILOBYTE
-        else  unsupported_unit!(unit)
-        end
+        to_gibi(size, byte_prefix(unit))
       end
 
       def to_megabytes(size, unit)
-        if    unit == :B then size.fdiv(MEGABYTE)
-        elsif unit == :KB then size.fdiv(KILOBYTE)
-        elsif unit == :GB then size * KILOBYTE
-        elsif unit == :MB then size
-        elsif unit == :TB then size * MEGABYTE
-        else  unsupported_unit!(unit)
-        end
+        to_mebi(size, byte_prefix(unit))
       end
 
       def to_kilobytes(size, unit)
-        if    unit == :B  then size.fdiv(KILOBYTE)
-        elsif unit == :KB then size
-        elsif unit == :MB then size * KILOBYTE
-        elsif unit == :GB then size * MEGABYTE
-        elsif unit == :TB then size * GIGABYTE
-        elsif unit == :sector then (size * 0.5).round # http://en.wikipedia.org/wiki/Disk_sector
-        else  unsupported_unit!(unit)
+        if unit == :sector
+          return (size * HALF_OF_SECTOR).round # http://en.wikipedia.org/wiki/Disk_sector
         end
+
+        to_kibi(size, byte_prefix(unit))
       end
 
       def to_bytes(size, unit)
-        if    unit == :B  then size
-        elsif unit == :KB then size * KILOBYTE
-        elsif unit == :MB then size * MEGABYTE
-        elsif unit == :GB then size * GIGABYTE
-        elsif unit == :TB then size * TERABYTE
-        else unsupported_unit!(unit)
-        end
+        to_binary_bi(size, byte_prefix(unit))
       end
 
       # Convert bytes to manually defined format
       #   by using parameter 'unit'
       #   ATTENTION: by default round is eq to 3 digits
       def bytes_to_human_size(size, unit, rounder = 3)
-        if    unit == :B  then size.round(rounder)
-        elsif unit == :KB then to_kilobytes(size, :B).round(rounder)
-        elsif unit == :MB then to_megabytes(size, :B).round(rounder)
-        elsif unit == :GB then to_gigabytes(size, :B).round(rounder)
-        elsif unit == :TB then to_terabytes(size, :B).round(rounder)
+        case unit
+        when :B  then size.round(rounder)
+        when :KB then to_kilobytes(size, :B).round(rounder)
+        when :MB then to_megabytes(size, :B).round(rounder)
+        when :GB then to_gigabytes(size, :B).round(rounder)
+        when :TB then to_terabytes(size, :B).round(rounder)
         else  unsupported_unit!(unit)
         end
       end
 
       private
+
+      def byte_prefix(unit)
+        case unit
+        when :B  then :B
+        when :KB then :KiB
+        when :MB then :MiB
+        when :GB then :GiB
+        when :TB then :TiB
+        else unsupported_unit!(unit)
+        end
+      end
 
       def unsupported_unit!(unit)
         raise ArgumentError.new("Unsupported unit - #{ unit }")
---
+++ lib/utils/size/cdn_byte.rb
@@ -0,0 +1,33 @@
+require 'utils/size/standard/decimal'
+
+module Utils
+  module Size
+    module CdnByte
+      # @note: used SI standard http://en.wikipedia.org/wiki/Binary_prefix
+      #   Decimal
+      #   1 kilobit = 1000 bit
+      include Utils::Size::Standard::Decimal
+
+      THOUSAND = 1_000
+
+      def to_cdn_gigabytes(size, unit)
+        #TODO check if it still in use
+        if unit == :Mbit
+          return size.fdiv(THOUSAND)/8
+        end
+
+        to_giga(size, unit)
+      end
+
+      def to_cdn_bytes(size, unit)
+        to_decimal_bi(size, unit)
+      end
+
+      private
+
+      def unsupported_unit!(unit)
+        raise ArgumentError.new("Unsupported unit - #{ unit }")
+      end
+    end
+  end
+end
