abaaeef45157d6e63042a74cf62432eea970ac90
Volodymyr Vysochanskyi <volodymyr.vysochanskyi@onapp.com>
2017-02-16 16:33:46 +0200

    Remove similar methods

--- lib/utils/size.rb
+++ lib/utils/size.rb
@@ -1,6 +1,6 @@
 require 'active_support/core_ext/numeric/bytes'
 require_relative 'size/bit'
-require_relative 'size/cdn_byte'
+require_relative 'size/cdn_speed'
 require_relative 'size/frequency'
 require_relative 'size/byte'
 require_relative 'size/percentage'
@@ -8,7 +8,7 @@ require_relative 'size/percentage'
 module Utils
   module Size
     include Utils::Size::Bit
-    include Utils::Size::CdnByte
+    include Utils::Size::CdnSpeed
     include Utils::Size::Frequency
     include Utils::Size::Byte
     include Utils::Size::Percentage
